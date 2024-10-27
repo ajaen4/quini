beaa723986dbd8e7657dcc1c7f180140c9623ed9
ajaen4 <a.jaenrev@gmail.com>
2024-10-27 12:10:37 +0100

    Fix: logger to show correctly locally and in lambda

--- container_imgs/data_uploader/f_data_uploader/logger.py
+++ container_imgs/data_uploader/f_data_uploader/logger.py
@@ -1,7 +1,10 @@
 import logging
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(
-    format="%(levelname)s: %(message)s",
-    level=logging.INFO
-)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+if not logger.handlers:
+    formatter = logging.Formatter("%(levelname)s: %(message)s")
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
