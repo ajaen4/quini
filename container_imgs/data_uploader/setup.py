978701923cb6e6783feab8e1b66075f8b6772bd4
ajaen4 <a.jaenrev@gmail.com>
2024-12-03 19:51:20 +0100

    Bump data_uploader: 0.0.36 → 0.0.37

--- container_imgs/data_uploader/setup.py
+++ container_imgs/data_uploader/setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="data_uploader",
-    version="0.0.36",
+    version="0.0.37",
     packages=find_packages(),
     install_requires=requirements,
 )
