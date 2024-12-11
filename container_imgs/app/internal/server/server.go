4d188e054f50f69c06ff081afec4475183407136
ajaen4 <a.jaenrev@gmail.com>
2024-12-11 21:07:58 +0100

    Move AWS Param fetch back to db

--- container_imgs/app/internal/server/server.go
+++ container_imgs/app/internal/server/server.go
@@ -1,10 +1,8 @@
 package server
 
 import (
-	"aws_lib/aws_lib"
 	"encoding/gob"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"time"
@@ -29,16 +27,6 @@ func NewServer(port int) *Server {
 	gob.Register(time.Time{})
 
 	env := os.Getenv("ENV")
-
-	if env != "LOCAL" {
-		sm := aws_lib.NewSSM("")
-		param := sm.GetParam(fmt.Sprintf("/quini/secrets/%s", env), true)
-		for varName, varValue := range param {
-			if err := os.Setenv(varName, varValue); err != nil {
-				log.Fatal("failed to set environment variable %s: %w", varName, err)
-			}
-		}
-	}
 
 	key := []byte(os.Getenv("SESSION_KEY"))
 	isSecure := env != "LOCAL"
