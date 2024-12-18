9d957030cdc6f59d150307d1f30c14ec2803886a
ajaen4 <a.jaenrev@gmail.com>
2024-12-18 19:04:07 +0100

    Add loterias headers

--- container_imgs/data_uploader/f_data_uploader/cfg.py
+++ container_imgs/data_uploader/f_data_uploader/cfg.py
@@ -35,3 +35,20 @@ LOTERO_URL = os.environ["LOTERO_URL"]
 LOTERO_TOKEN = secrets["LOTERO_TOKEN"]
 FOOT_API_URL = os.environ["FOOT_API_URL"]
 FOOT_API_TOKEN = secrets["FOOT_API_TOKEN"]
+
+LOTERO_HEADERS = {
+    "accept": "application/json, text/javascript, */*; q=0.01",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "es-ES,es;q=0.9,en;q=0.8",
+    "content-type": "application/json",
+    "dnt": "1",
+    "origin": "https://juegos.loteriasyapuestas.es",
+    "referer": "https://juegos.loteriasyapuestas.es/",
+    "sec-ch-ua": '"Chromium";v="131", "Not_A Brand";v="24"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"macOS"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+}
