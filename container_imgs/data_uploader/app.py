a4529371e1e4990c8e4f68cda3acebb1c4395a1b
ajaen4 <a.jaenrev@gmail.com>
2024-12-18 19:56:27 +0100

    Add logging for unhandled exceptions

--- container_imgs/data_uploader/app.py
+++ container_imgs/data_uploader/app.py
@@ -1,5 +1,10 @@
 from f_data_uploader.actions import run_data_uploader
+from f_data_uploader.logger import logger
 
 
 def handler(event, context):
-    run_data_uploader()
+    try:
+        run_data_uploader()
+    except Exception as e:
+        logger.exception("Unhandled exception: %s", str(e))
+        raise
